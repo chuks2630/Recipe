26135994b4db33102ea51f88f0c1a0e8ad4cf084
chuks <tstorm2630@gmail.com>
2024-09-11 14:34:12 +0100

    first recipe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import Banner from "./Recipe_components/Banner";
+import Copyright from "./Recipe_components/Copyright";
+import Nav from "./Recipe_components/Nav";
+import Footer from "./Recipe_components/Footer";
+import List from "./Recipe_components/List";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from "axios";
+import { useEffect, useState } from "react";
+const RecipeApp = ()=>{
+
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const [recipes, setRecipes] = useState([]);
+    const [keyword, setKeyword] = useState('');
+    const [frecipe, setFRecipe] = useState([]);
+
+    useEffect(()=>{
+       makeApiCall()
+    }, [])
+
+    //this function will do the filtering
+   const handleFilter = ()=>{
+    const filtered_recipes = recipes.filter((recipe)=>{
+        return recipe.name.toLowerCase().includes(keyword.toLowerCase())
+    })
+
+    setFRecipe(filtered_recipes)
+   }
+
+    function makeApiCall(){
+        axios.get("https://dummyjson.com/recipes")
+        .then(function(resp){
+            console.log(resp.data.recipes)
+            setLoading(false)
+            setRecipes(resp.data.recipes)
+        })
+        .catch(function(err){
+            console.log(err)
+            setLoading(false);
+            setError(true)
+            
+        })
+       
+    }
+    return (
+        <div className="container-fluid">
+            <Nav/>
+            <Banner setKeyword = {setKeyword} keyword={keyword} handleFilter={handleFilter}/>
+            <List loading = {loading} error = {error} recipes = {recipes} frecipe= {frecipe} keyword={keyword}/>
+            <Footer/>
+            <Copyright/>
+        </div>
+    )
 }
 
-export default App;
+export default RecipeApp;
---
+++ src/Recipe_components/Copyright.js
@@ -0,0 +1,13 @@
+const Copyright = ()=>{
+    return(
+        <>
+            <div className="row bg-dark text-white">
+                <div className="col">
+                    <p className="text-center my-3"> © 2024. Cohort 28 Class Project</p>
+                </div>
+            </div>
+
+        </>
+    )
+}
+export default Copyright;
